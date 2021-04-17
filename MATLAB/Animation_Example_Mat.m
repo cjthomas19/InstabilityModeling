61bb8f5b341b1c97995d56f8d1a8417a1f475f7c
Chase Thomas <cj.thomas19@students.bchigh.edu>
2021-04-17 18:01:20 -0400

    Changed to Matlab Script
    LiveScript runs much slower.

---
+++ MATLAB/Animation_Example_Mat.m
@@ -0,0 +1,30 @@
+syms t th z(th, t)
+theta = linspace(0, 2*pi, 10);
+
+x = cos(theta);
+y = sin(theta);
+
+z(th, t) = cos(t*th)
+
+
+h = figure;
+axis([-1,1,-1,1,-1,1])
+
+
+ax = gca;
+ax.NextPlot = "replaceChildren";
+
+v = VideoWriter('newfile2.avi','Uncompressed AVI');
+open(v);
+
+
+loops = 40;
+
+for i = 0:loops
+    plot3(x, y, z(theta, i/20))
+    writeVideo(v,getframe(h));
+end
+
+close(v);
+
+
