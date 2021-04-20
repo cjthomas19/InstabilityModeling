8d16369e3c819fcf479ad2a1bf92b30034c1d725
sperangu <61212207+sperangu@users.noreply.github.com>
2021-04-20 15:30:08 -0400

    Update Inequality_modeling.m

--- MATLAB/Inequality_modeling.m
+++ MATLAB/Inequality_modeling.m
@@ -6,8 +6,8 @@ clear, close
 %decide n,m
 m = .5;
 n=2.5;
-b=1;
-c=-3;
+b=2.5;
+c=-1.25;
 %alternatively, ask user:
 % m=input('Please enter a value for mu: ');
 % n=input('Please enter a value for nu: ');
@@ -28,9 +28,16 @@ H(range) = NaN; %hides all values where H=0
 G = real(ax*n+dx*m);
 figure
 J=G-H;
-s1 = surf(ax,dx,J); %graphs upper boundary surface 
-axis([min(a),max(a),min(d),max(d),0,1.25*max(max(H))]);
-s1.EdgeColor = 'none'; %or else it displays black
+s3 = surf(ax,dx,J); %graphs surface 
+axis([0,max(a),min(d),max(d),0,1.25*max(max(H))]);
+s3.EdgeColor = 'none'; %or else it displays black
+xlabel('a','FontWeight', 'bold', 'FontSize', 13)
+ylabel('d','FontWeight', 'bold', 'FontSize', 13);
+zlabel('J = a\nu + d\mu - 2(\mu\nu(ad-bc))^{1/2}','FontWeight', 'bold', 'FontSize', 13);
+title('Allowed values of a,d Given \mu=.5, \nu=2.5, b=2.5, c=-1.25');
 colormap winter
-%s2=surf(ax,dx,G);
-%s2.EdgeColor='none'; %for graphing the plane separately
+hold on
+% s2=surf(ax,dx,G);
+% s1=surf(ax,dx,H);
+% s2.EdgeColor='none'; %for graphing the plane separately
+% s1.EdgeColor='none';
